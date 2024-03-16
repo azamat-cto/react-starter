1edec97fa8c3856343515ddd557532e9f2b07904
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-03-17 03:08:38 +0500

    Add container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+/** @type {import('tailwindcss').Config} */
 const config = {
     content: ["./index.html", "./src/**/*.{js,jsx}"],
     theme: {
+        container: {
+            center: true,
+            padding: "1rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
         fontFamily: {
             sans: ['"Roboto"', ...fontFamily.sans],
         },
