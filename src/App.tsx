aef2a600b978f4c22a0715962169ba7171715b05
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-11 22:25:15 +0500

    add `badge` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "./components/ui/badge";
 
 function App() {
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto">
-            <Avatar>
-                <AvatarImage
-                    alt="@shadcn"
-                    src="https://github.com/shadcn.png"
-                />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            <Badge>Badge</Badge>
         </div>
     );
 }
