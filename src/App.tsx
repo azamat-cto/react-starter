4c81be632f4deaec92fd91f08798cf07109fe8c7
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-11 22:14:32 +0500

    add `button` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { Terminal } from "lucide-react";
-
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "./components/ui/button";
 
 function App() {
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto">
-            <Alert>
-                <Terminal className="h-4 w-4" />
-                <AlertTitle>Heads up!</AlertTitle>
-                <AlertDescription>
-                    You can add components to your app using the cli.
-                </AlertDescription>
-            </Alert>
+            <Button>Button</Button>
         </div>
     );
 }
