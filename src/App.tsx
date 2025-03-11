7e5d447c1d3c19e38d168b11d231f044b6a1663a
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-11 22:10:39 +0500

    add `alert` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "./components/ui/accordion";
+import { Terminal } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 function App() {
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto">
-            <Accordion type="single" collapsible className="w-full">
-                <AccordionItem value="item-1">
-                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                    <AccordionContent>
-                        Yes. It adheres to the WAI-ARIA design pattern.
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                    <AccordionTrigger>Is it styled?</AccordionTrigger>
-                    <AccordionContent>
-                        Yes. It comes with default styles that matches the other
-                        components&apos; aesthetic.
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                    <AccordionTrigger>Is it animated?</AccordionTrigger>
-                    <AccordionContent>
-                        Yes. It's animated by default, but you can disable it if
-                        you prefer.
-                    </AccordionContent>
-                </AccordionItem>
-            </Accordion>
+            <Alert>
+                <Terminal className="h-4 w-4" />
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    You can add components to your app using the cli.
+                </AlertDescription>
+            </Alert>
         </div>
     );
 }
