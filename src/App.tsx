3cfeb0cc812f1130af11945fb959998ab67f1463
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-12 10:02:28 +0500

    add `card` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
-import { useState } from "react";
-import { Calendar } from "@/components/ui/calendar";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Button } from "./components/ui/button";
 
 function App() {
-    const [date, setDate] = useState<Date | undefined>(new Date());
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto">
-            <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                className="rounded-md border shadow"
-            />
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-lg font-semibold">
+                        Card Title
+                    </CardTitle>
+                    <CardDescription className="leading-snug text-muted-foreground">
+                        Lorem ipsum dolor sit amet consectetur.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <img
+                        className="h-40 w-full object-cover object-center"
+                        src="https://shadcnblocks.com/images/block/placeholder-1.svg"
+                        alt="placeholder"
+                    />
+                </CardContent>
+                <CardFooter>
+                    <Button className="w-full" variant="outline">
+                        Read More
+                    </Button>
+                </CardFooter>
+            </Card>
         </div>
     );
 }
