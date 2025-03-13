df01ffaa24ad61adf8a7d9141fd26747075dfb8b
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-13 20:57:25 +0500

    add `dialog` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover";
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 import { Button } from "./components/ui/button";
 
 function App() {
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto">
-            <Popover>
-                <PopoverTrigger asChild>
+            <Dialog>
+                <DialogTrigger asChild>
                     <Button variant="outline">Open</Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                    Place content for the popover here.
-                </PopoverContent>
-            </Popover>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogDescription>
+                            This action cannot be undone. This will permanently
+                            delete your account and remove your data from our
+                            servers.
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
         </div>
     );
 }
