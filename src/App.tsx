2e626fb059ff9135600f4f29128da8c115f9a393
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-13 20:51:12 +0500

    add `popover` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,21 @@
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
 import {
-    Collapsible,
-    CollapsibleContent,
-    CollapsibleTrigger,
-} from "@/components/ui/collapsible";
-import { ChevronsUpDown } from "lucide-react";
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "./components/ui/button";
 
 function App() {
-    const [isOpen, setIsOpen] = useState(false);
-
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto">
-            <Collapsible
-                open={isOpen}
-                onOpenChange={setIsOpen}
-                className="w-[350px] space-y-2"
-            >
-                <div className="flex items-center justify-between space-x-4 px-4">
-                    <h4 className="text-sm font-semibold">
-                        @peduarte starred 3 repositories
-                    </h4>
-                    <CollapsibleTrigger asChild>
-                        <Button variant="ghost" size="sm">
-                            <ChevronsUpDown className="h-4 w-4" />
-                            <span className="sr-only">Toggle</span>
-                        </Button>
-                    </CollapsibleTrigger>
-                </div>
-                <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm">
-                    @radix-ui/primitives
-                </div>
-                <CollapsibleContent className="space-y-2">
-                    <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm">
-                        @radix-ui/colors
-                    </div>
-                    <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm">
-                        @stitches/react
-                    </div>
-                </CollapsibleContent>
-            </Collapsible>
+            <Popover>
+                <PopoverTrigger asChild>
+                    <Button variant="outline">Open</Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                    Place content for the popover here.
+                </PopoverContent>
+            </Popover>
         </div>
     );
 }
