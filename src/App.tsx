63714d1377eefac5e6a07b6a1e012cf86f76c0ec
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-13 21:02:07 +0500

    add `command` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,65 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog";
-import { Button } from "./components/ui/button";
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+} from "@/components/ui/command";
+import {
+    Calculator,
+    Calendar,
+    CreditCard,
+    Settings,
+    Smile,
+    User,
+} from "lucide-react";
 
 function App() {
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto">
-            <Dialog>
-                <DialogTrigger asChild>
-                    <Button variant="outline">Open</Button>
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Are you absolutely sure?</DialogTitle>
-                        <DialogDescription>
-                            This action cannot be undone. This will permanently
-                            delete your account and remove your data from our
-                            servers.
-                        </DialogDescription>
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
+            <div className="h-80">
+                <Command className="rounded-lg border shadow-md md:min-w-[450px]">
+                    <CommandInput placeholder="Type a command or search..." />
+                    <CommandList>
+                        <CommandEmpty>No results found.</CommandEmpty>
+                        <CommandGroup heading="Suggestions">
+                            <CommandItem>
+                                <Calendar />
+                                <span>Calendar</span>
+                            </CommandItem>
+                            <CommandItem>
+                                <Smile />
+                                <span>Search Emoji</span>
+                            </CommandItem>
+                            <CommandItem disabled>
+                                <Calculator />
+                                <span>Calculator</span>
+                            </CommandItem>
+                        </CommandGroup>
+                        <CommandSeparator />
+                        <CommandGroup heading="Settings">
+                            <CommandItem>
+                                <User />
+                                <span>Profile</span>
+                                <CommandShortcut>⌘P</CommandShortcut>
+                            </CommandItem>
+                            <CommandItem>
+                                <CreditCard />
+                                <span>Billing</span>
+                                <CommandShortcut>⌘B</CommandShortcut>
+                            </CommandItem>
+                            <CommandItem>
+                                <Settings />
+                                <span>Settings</span>
+                                <CommandShortcut>⌘S</CommandShortcut>
+                            </CommandItem>
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </div>
         </div>
     );
 }
