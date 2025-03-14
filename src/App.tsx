7abd6ac91ed70d40a90f6828cec0b2893b78ce17
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-14 12:28:31 +0500

    add `textarea` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
+import { Textarea } from "@/components/ui/textarea";
 
 function App() {
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto pt-4">
-            <div className="flex items-center space-x-2">
-                <Switch id="airplane-mode" />
-                <Label htmlFor="airplane-mode">Airplane Mode</Label>
-            </div>
+            <Textarea placeholder="Type your message here." />
         </div>
     );
 }
