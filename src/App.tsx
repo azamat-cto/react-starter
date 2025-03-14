5e5983364c9d4cdcc314ac08e31e0f5091b57728
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-14 12:25:45 +0500

    add `slider` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
+import { Slider } from "@/components/ui/slider";
+
+type SliderProps = React.ComponentProps<typeof Slider>;
 
 function App() {
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto pt-4">
-            <div className="flex items-center space-x-4">
-                <Skeleton className="h-12 w-12 rounded-full" />
-                <div className="space-y-2">
-                    <Skeleton className="h-4 w-[250px]" />
-                    <Skeleton className="h-4 w-[200px]" />
-                </div>
-            </div>
+            <SliderDemo />
         </div>
+    );
+}
+
+function SliderDemo({ className, ...props }: SliderProps) {
+    return (
+        <Slider
+            defaultValue={[50]}
+            max={100}
+            step={1}
+            className={cn("w-[60%]", className)}
+            {...props}
+        />
     );
 }
 
