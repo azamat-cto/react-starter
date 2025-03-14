4fa36474e7c9869391342833fa2ee8ccd2f4b69f
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-14 12:32:31 +0500

    add `toggle-group` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { Toggle } from "@/components/ui/toggle";
-import { Bold } from "lucide-react";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Bold, Italic, Underline } from "lucide-react";
 
 function App() {
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto pt-4">
-            <Toggle aria-label="Toggle italic">
-                <Bold className="h-4 w-4" />
-            </Toggle>
+            <ToggleGroup type="multiple">
+                <ToggleGroupItem value="bold" aria-label="Toggle bold">
+                    <Bold className="h-4 w-4" />
+                </ToggleGroupItem>
+                <ToggleGroupItem value="italic" aria-label="Toggle italic">
+                    <Italic className="h-4 w-4" />
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                    value="strikethrough"
+                    aria-label="Toggle strikethrough"
+                >
+                    <Underline className="h-4 w-4" />
+                </ToggleGroupItem>
+            </ToggleGroup>
         </div>
     );
 }
