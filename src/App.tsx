3471d74b614f0c97a709b368617549cb40320087
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-14 12:27:05 +0500

    add `switch` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import { cn } from "@/lib/utils";
-import { Slider } from "@/components/ui/slider";
-
-type SliderProps = React.ComponentProps<typeof Slider>;
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 
 function App() {
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto pt-4">
-            <SliderDemo />
+            <div className="flex items-center space-x-2">
+                <Switch id="airplane-mode" />
+                <Label htmlFor="airplane-mode">Airplane Mode</Label>
+            </div>
         </div>
-    );
-}
-
-function SliderDemo({ className, ...props }: SliderProps) {
-    return (
-        <Slider
-            defaultValue={[50]}
-            max={100}
-            step={1}
-            className={cn("w-[60%]", className)}
-            {...props}
-        />
     );
 }
 
