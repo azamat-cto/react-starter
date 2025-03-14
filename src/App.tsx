db7c4c23910aa3f0ee64bee70ba7643d4c657a0d
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-14 12:18:40 +0500

    add `select` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import { useEffect, useState } from "react";
-import { Progress } from "@/components/ui/progress";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 
 function App() {
-    const [progress, setProgress] = useState(13);
-
-    useEffect(() => {
-        const timer = setTimeout(() => setProgress(66), 2000);
-        return () => clearTimeout(timer);
-    }, []);
-
     return (
         <div className="flex flex-col items-center h-svh max-w-2xl mx-auto pt-4">
-            <Progress value={progress} className="w-[60%]" />
+            <Select>
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select a fruit" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectLabel>Fruits</SelectLabel>
+                        <SelectItem value="apple">Apple</SelectItem>
+                        <SelectItem value="banana">Banana</SelectItem>
+                        <SelectItem value="blueberry">Blueberry</SelectItem>
+                        <SelectItem value="grapes">Grapes</SelectItem>
+                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
         </div>
     );
 }
