a699eac541a2df97caff1db834cbfc82b79e51dc
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-14 12:33:49 +0500

    add `tooltip` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { Bold, Italic, Underline } from "lucide-react";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
 
 function App() {
     return (
         <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto pt-4">
-            <ToggleGroup type="multiple">
-                <ToggleGroupItem value="bold" aria-label="Toggle bold">
-                    <Bold className="h-4 w-4" />
-                </ToggleGroupItem>
-                <ToggleGroupItem value="italic" aria-label="Toggle italic">
-                    <Italic className="h-4 w-4" />
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                    value="strikethrough"
-                    aria-label="Toggle strikethrough"
-                >
-                    <Underline className="h-4 w-4" />
-                </ToggleGroupItem>
-            </ToggleGroup>
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Button variant="outline">Hover</Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>Add to library</p>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
         </div>
     );
 }
