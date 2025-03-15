b5ab280d950360e5fd5108f36c69da3fecbd65c5
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-03-15 13:36:08 +0500

    add `tabs` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import { toast } from "sonner";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function App() {
-    toast("Event has been created.");
-
     return (
         <>
             <div className="flex flex-col items-center justify-center h-svh">
-                <Button
-                    variant="outline"
-                    onClick={() =>
-                        toast("Event has been created", {
-                            description: "Sunday, December 03, 2023 at 9:00 AM",
-                            action: {
-                                label: "Undo",
-                                onClick: () => console.log("Undo"),
-                            },
-                        })
-                    }
-                >
-                    Show Toast
-                </Button>
+                <Tabs defaultValue="account" className="w-[400px]">
+                    <TabsList className="grid w-full grid-cols-2">
+                        <TabsTrigger value="account">Account</TabsTrigger>
+                        <TabsTrigger value="password">Password</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="account">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Account</CardTitle>
+                                <CardDescription>
+                                    Make changes to your account here. Click
+                                    save when you're done.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="space-y-2">
+                                <div className="space-y-1">
+                                    <Label htmlFor="name">Name</Label>
+                                    <Input id="name" />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="username">Username</Label>
+                                    <Input id="username" />
+                                </div>
+                            </CardContent>
+                            <CardFooter>
+                                <Button>Save changes</Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                    <TabsContent value="password">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Password</CardTitle>
+                                <CardDescription>
+                                    Change your password here. After saving,
+                                    you'll be logged out.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="space-y-2">
+                                <div className="space-y-1">
+                                    <Label htmlFor="current">
+                                        Current password
+                                    </Label>
+                                    <Input id="current" type="password" />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="new">New password</Label>
+                                    <Input id="new" type="password" />
+                                </div>
+                            </CardContent>
+                            <CardFooter>
+                                <Button>Save password</Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                </Tabs>
             </div>
         </>
     );
