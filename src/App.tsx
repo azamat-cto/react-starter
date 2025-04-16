bbc4c92b81a93535cf45f76aa527a26d816947f9
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-04-16 16:49:04 +0500

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,22 @@ import * as React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@/components/ui/sonner";
-import { UserTable } from "@/components/user/user-table";
+
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "./components/ui/breadcrumb";
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "./components/ui/sidebar";
+import { Separator } from "./components/ui/separator";
+import { AppSidebar } from "./components/app-sidebar";
 
 const queryClient = new QueryClient();
 const ReactQueryDevtoolsProduction = React.lazy(() =>
@@ -22,11 +37,41 @@ function App() {
     return (
         <>
             <QueryClientProvider client={queryClient}>
-                <div className="flex flex-col items-center justify-center h-svh max-w-2xl mx-auto">
-                    <div className="w-full overflow-x-auto">
-                        <UserTable />
-                    </div>
-                </div>
+                <SidebarProvider>
+                    <AppSidebar />
+                    <SidebarInset>
+                        <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
+                            <SidebarTrigger className="-ml-1" />
+                            <Separator
+                                orientation="vertical"
+                                className="mr-2 h-4"
+                            />
+                            <Breadcrumb>
+                                <BreadcrumbList>
+                                    <BreadcrumbItem className="hidden md:block">
+                                        <BreadcrumbLink href="#">
+                                            Building Your Application
+                                        </BreadcrumbLink>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator className="hidden md:block" />
+                                    <BreadcrumbItem>
+                                        <BreadcrumbPage>
+                                            Data Fetching
+                                        </BreadcrumbPage>
+                                    </BreadcrumbItem>
+                                </BreadcrumbList>
+                            </Breadcrumb>
+                        </header>
+                        <div className="flex flex-1 flex-col gap-4 p-4">
+                            {Array.from({ length: 24 }).map((_, index) => (
+                                <div
+                                    key={index}
+                                    className="aspect-video h-12 w-full rounded-lg bg-muted/50"
+                                />
+                            ))}
+                        </div>
+                    </SidebarInset>
+                </SidebarProvider>
                 <Toaster />
                 <ReactQueryDevtools initialIsOpen={false} />
                 {showDevtools && (
